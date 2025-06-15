ba053a3534e86bc9d20274dba3635e15461dd172
aimane <120175452+rhiminiaimane@users.noreply.github.com>
2025-06-16 00:00:22 +0100

    sitemap

--- pages/api/sitemap.ts
+++ pages/api/sitemap.ts
@@ -1,23 +1,17 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
+const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://autospatanger.com/</loc>
+    <lastmod>${new Date().toISOString()}</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+  </url>
+  <!-- Ajoutez ici d’autres URLs de votre site -->
+</urlset>`;
+
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   res.setHeader('Content-Type', 'application/xml');
-  res.write(
-    `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
-      <url>
-        <loc>https://autospatanger.com/</loc>
-      </url>
-      <url>
-        <loc>https://autospatanger.com/services</loc>
-      </url>
-      <url>
-        <loc>https://autospatanger.com/galerie</loc>
-      </url>
-      <url>
-        <loc>https://autospatanger.com/contact</loc>
-      </url>
-    </urlset>`
-  );
-  res.end();
+  res.status(200).send(sitemap);
 }
